3d7377a8b20ebac5ab420109c66470e5e97aa7ec
Rachel <rzhangmoz1@gmail.com>
2018-04-13 15:17:26 -0700

    tiny change on the layout

--- colorGame.js
+++ colorGame.js
@@ -1,9 +1,9 @@
 
 var numSquares = 6
 var colors = [];
+var pickedColor;
 
 var squares = document.querySelectorAll(".square");
-var pickedColor;
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message")
 var h1 = document.querySelector("h1");
